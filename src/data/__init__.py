e61c224bb43848a5d163f8380b8399afcddd364f
Yaroslav Vasylenko <stapkabenderka@ukr.net>
2025-10-15 18:12:44 +0300

    Add caching ingestion service and tests

---
+++ src/data/__init__.py
@@ -0,0 +1,8 @@
+"""High-level data ingestion services and helpers."""
+
+from src.data.ingestion_service import CacheEntrySnapshot, DataIngestionCacheService
+
+__all__ = [
+    "CacheEntrySnapshot",
+    "DataIngestionCacheService",
+]
