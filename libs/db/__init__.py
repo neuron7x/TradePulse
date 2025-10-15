dd7ec6b2f24742fd79edb0777eb6fd22586bb082
Yaroslav Vasylenko <stapkabenderka@ukr.net>
2025-10-16 01:59:44 +0300

    Add data access layer abstraction

--- libs/db/__init__.py
+++ libs/db/__init__.py
@@ -1,6 +1,7 @@
-"""Database connection helpers."""
+"""Database connection helpers and abstractions."""
 
+from .access import DataAccessLayer
 from .postgres import create_postgres_connection
 
-__all__ = ["create_postgres_connection"]
+__all__ = ["create_postgres_connection", "DataAccessLayer"]
 
