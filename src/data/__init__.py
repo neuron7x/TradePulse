027909ca8072fd33616b2ec10801d1ef28c12066
Yaroslav Vasylenko <stapkabenderka@ukr.net>
2025-10-16 17:35:25 +0300

    Add Kafka ingestion service with exactly-once guarantees

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,11 +1,25 @@
 """High-level data ingestion services and helpers."""
 
 from src.data.ingestion_service import CacheEntrySnapshot, DataIngestionCacheService
+from src.data.kafka_ingestion import (
+    HotSymbolCache,
+    HotSymbolSnapshot,
+    KafkaIngestionConfig,
+    KafkaIngestionService,
+    LagReport,
+    LagRecord,
+)
 from src.data.streaming_aggregator import AggregationResult, TickStreamAggregator
 
 __all__ = [
     "AggregationResult",
     "CacheEntrySnapshot",
     "DataIngestionCacheService",
+    "HotSymbolCache",
+    "HotSymbolSnapshot",
+    "KafkaIngestionConfig",
+    "KafkaIngestionService",
+    "LagRecord",
+    "LagReport",
     "TickStreamAggregator",
 ]
