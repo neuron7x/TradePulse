69962b0b571e45976eb8d0ecfb74bbf8df2fcd7e
Yaroslav Vasylenko <stapkabenderka@ukr.net>
2025-10-16 14:44:42 +0300

    Fix provider configuration compatibility

--- infra/terraform/eks/main.tf
+++ infra/terraform/eks/main.tf
@@ -114,7 +114,7 @@ data "aws_caller_identity" "current" {}
 data "aws_partition" "current" {}
 
 data "aws_eks_cluster_auth" "this" {
-  name = module.eks.cluster_name
+  cluster_name = module.eks.cluster_name
 }
 
 locals {
