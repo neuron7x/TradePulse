e1884039ac5cc7259f31c3197af661e327e295b1
Yaroslav Vasylenko <stapkabenderka@ukr.net>
2025-10-17 23:18:05 +0300

    Add liquidity impact modeling utilities

--- analytics/__init__.py
+++ analytics/__init__.py
@@ -1,1 +1,20 @@
 # SPDX-License-Identifier: MIT
+"""Analytics helpers exposed as part of the public package API."""
+
+from .liquidity_impact import (  # noqa: F401
+    ExecutionForecast,
+    ExecutionParameters,
+    LiquidityImpactConfig,
+    LiquidityImpactModel,
+    LiquiditySnapshot,
+    OrderBookLevel,
+)
+
+__all__ = [
+    "ExecutionForecast",
+    "ExecutionParameters",
+    "LiquidityImpactConfig",
+    "LiquidityImpactModel",
+    "LiquiditySnapshot",
+    "OrderBookLevel",
+]
