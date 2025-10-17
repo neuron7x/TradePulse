b87ac0ae11b8f9ade692cc650d16ec96ee6a5114
Yaroslav Vasylenko <stapkabenderka@ukr.net>
2025-10-18 00:24:38 +0300

    Add environment parity checks across environments

--- analytics/__init__.py
+++ analytics/__init__.py
@@ -1,6 +1,17 @@
 # SPDX-License-Identifier: MIT
 """Analytics helpers exposed as part of the public package API."""
 
+from .environment_parity import (  # noqa: F401
+    EnvironmentParityChecker,
+    EnvironmentParityConfig,
+    EnvironmentParityError,
+    EnvironmentParityReport,
+    MetricDeviation,
+    MetricTolerance,
+    StrategyRunSnapshot,
+    compute_code_digest,
+    compute_parameters_digest,
+)
 from .liquidity_impact import (  # noqa: F401
     ExecutionForecast,
     ExecutionParameters,
@@ -11,10 +22,19 @@ from .liquidity_impact import (  # noqa: F401
 )
 
 __all__ = [
+    "EnvironmentParityChecker",
+    "EnvironmentParityConfig",
+    "EnvironmentParityError",
+    "EnvironmentParityReport",
+    "MetricDeviation",
+    "MetricTolerance",
+    "StrategyRunSnapshot",
     "ExecutionForecast",
     "ExecutionParameters",
     "LiquidityImpactConfig",
     "LiquidityImpactModel",
     "LiquiditySnapshot",
     "OrderBookLevel",
+    "compute_code_digest",
+    "compute_parameters_digest",
 ]
